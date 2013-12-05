789bc0408e776302a2a06864180ed699bb51f4d1
José Escalante <jose@sumavisos.com>
2013-12-05 20:11:38 -0300

    Nuevos graficos!

--- codigo/exp_grafico_funciones.py
+++ codigo/exp_grafico_funciones.py
@@ -1,29 +1,32 @@
 # el proposito de este experimento es graficar f(x) y e(x) para un alpha
 # determinado
 
-from matplotlib import pyplot as plt
+from pylab import *
+import sys
 import numpy as np
 from tp1 import e, f
 
 
 x = np.arange(-2, 2, 0.05)
+x2 = np.arange(0, 5, 0.05)
 
 f_5 = x**2 - .05
 f1 = x**2 - 1
 f2 = x**2 - 2
 
-e_5 = 1 / (x**2) - .05
-e1 = 1 / (x**2) - 1
-e2 = 1 / (x**2) - 2
+e_5 = 1 / (x2**2) - .05
+e1 = 1 / (x2**2) - 1
+e2 = 1 / (x2**2) - 2
 
 plt.figure(1)
 # plt.title("")
 plt.xlabel('x')
 plt.ylabel('f(x)')
 plt.grid(True)
-plt.plot(x, f_5)
-plt.plot(x, f1)
-plt.plot(x, f2)
+plt.plot(x, f_5, label="u'\u03b1 = 0.5")
+plt.plot(x, f1, label="u'\u03b1 = 1")
+plt.plot(x, f2, label="u'\u03b1 = 2")
+legend(framealpha=0.5)
 
 plt.figure(2)
 # plt.title("e(x)")
@@ -31,9 +34,10 @@ plt.axis([-2, 2, -4, 20])
 plt.xlabel('x')
 plt.ylabel('e(x)')
 plt.grid(True)
-plt.plot(x, e_5)
-plt.plot(x, e1)
-plt.plot(x, e2)
+plt.plot(x2, e_5, label="u'\u03b1 = 0.5")
+plt.plot(x2, e1, label="u'\u03b1 = 1")
+plt.plot(x2, e2, label="u'\u03b1 = 2")
+legend(framealpha=0.5)
 
 plt.show()
 
